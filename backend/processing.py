d8d450a5ce4746a8dc3143c76fdcb8b5f51e88a4
Alexander Blake <alex.blake24@gmail.com>
2024-09-22 22:20:17 -0700

    Added comments to program

--- backend/processing.py
+++ backend/processing.py
@@ -10,12 +10,24 @@ CONSTANTS_FILE = 'constants.env'
 TICKET_API_KEY, FLIGHT_API_KEY = decrypt_file(CONSTANTS_FILE, get_key(KEY_FILE))
 
 def get_flight_info(origin: str, destination: str, start_date: str, end_date: str):
+    """
+    :param origin: 3-letter origin airport code
+    :param destination: 3-letter destination airport code
+    :param start_date: Date of outbound flight (%mm-%dd-%YYYY)
+    :param end_date: Date of inbound flight (%mm-%dd-%YYYY)
+    :return: SerpAPI response in JSON format
+    """
+
     with urllib.request.urlopen('https://serpapi.com/search.json?engine=google_flights&output=json&departure_id=' + origin + '&arrival_id=' + destination + '&outbound_date=' + start_date + '&return_date=' + end_date + '&api_key=' + FLIGHT_API_KEY) as url:
         flights = json.load(url)
 
     return flights
 
-def get_final_result(origin: str) -> list:
+def get_total_price(origin: str) -> list:
+    """
+    :param origin: 3-letter origin airport code
+    :return: A sorted list of all the total prices (flight + ticket)
+    """
     result = []
 
     with open(TEST_FILE, 'r') as file:
@@ -39,4 +51,4 @@ def get_final_result(origin: str) -> list:
     return result
 
 if __name__ == "__main__":
-    print(get_final_result('LAS'))
+    print(get_total_price('LAS'))
