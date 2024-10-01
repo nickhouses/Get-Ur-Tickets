c7aca53a5d4bb0024a5ac3b6d342943d98d77889
B1gbuddy <143768960+B1gbuddy@users.noreply.github.com>
2024-09-30 19:36:07 -0700

    Fixed authentication with AWS server

---
+++ get-ur-tickets/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "geturtickets3cb31893": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
