744d908b041a3375099a75c50af099cad3773c4a
BJones47 <braxtonjones21@gmail.com>
2024-10-19 13:40:15 -0700

    Imported banner to contact page with a redirect to home.

--- get-ur-tickets/src/pages/Contact Us.js
+++ get-ur-tickets/src/pages/Contact Us.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MyButton from "../Components/MyButton"; 
+import UserBanner from './Home'          
+import Fly_Now from "../Pictures/Fly_Now.png";
 
 export function Contact() {
+  const [showButton, setShowButton] = useState(true);
+
+  // Function to toggle buttons visibility
+  const toggleButton = () => {
+    setShowButton(!showButton);
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='banner-container'>
+      {/* Banner Image */}
+      <img alt="Website Banner of Hotel/Flight/Concert" className="Top-Banner" src={Fly_Now} />
+
+      {/* Button overlay and User Banner */}
+      <div className='button-overlay'>
+        {/* Button to show or hide the Contact/Signout buttons */}
+        <button className="Top-Banner-2" onClick={toggleButton} />
+
+        {/* Conditionally show buttons */}
+        {!showButton && (
+          <div className='button-group'>
+            <MyButton to="Home" />
+            <MyButton to="signout" />
+          </div>
+        )}
+
+        {/* User Banner displaying welcome message */}
+        <UserBanner />
+      </div>
     </div>
   );
-};
-
+}
