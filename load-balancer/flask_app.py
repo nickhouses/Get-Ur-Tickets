f8f614b6dd668f50033ae92f592a8904eada0f37
Alexander Blake <alex.blake24@gmail.com>
2024-11-15 12:27:47 -0800

    Load Balancer

---
+++ load-balancer/flask_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+COUNTER = 0
+SERVERS = ["https://geturtickets.pythonanywhere.com/", "https://geturticket.pythonanywhere.com/"]
+
+
+@app.route('/', methods=['GET'])
+def round_robin():
+    global COUNTER
+    result = SERVERS[COUNTER % len(SERVERS)]
+    COUNTER += 1
+    return jsonify({"baseURL": result})
+
+
+def create_app():
+    return app
+
+if __name__ == '__main__':
+    app.run(debug=True)
